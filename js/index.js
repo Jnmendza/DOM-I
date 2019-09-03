289483ea6846fe98d3b40d13e915cfbd740886ca
Jnmendza <jonatmendoza@gmail.com>
2019-09-02 21:02:47 -0700

    Finished task 2

--- js/index.js
+++ js/index.js
@@ -64,6 +64,41 @@ const button = document.querySelector('.cta .cta-text button')
 button.textContent = 'Get Started'
 
 // IMAGE
-const mainImg = document.querySelector('.cta img')
+const mainImg = document.getElementById('cta-img')
 mainImg.setAttribute('src', 'img/header-img.png')
 
+//TOP CONTENT FEATURES AND ABOUT
+const topContentH4 = document.querySelectorAll('.top-content .text-content h4')
+topContentH4[0].textContent = 'Features'
+topContentH4[1].textContent = 'About'
+
+const topContentP = document.querySelectorAll('.top-content .text-content p')
+topContentP[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+topContentP[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const middleImg = document.querySelector('.middle-img')
+middleImg.setAttribute('src', 'img/mid-page-accent.jpg')
+
+//BOTTOM CONTENT SERVICES, PRODUCT AND VISION
+const bottomContentH4 = document.querySelectorAll('.bottom-content .text-content h4')
+bottomContentH4[0].textContent = 'Services'
+bottomContentH4[1].textContent = 'Product'
+bottomContentH4[2].textContent = 'Vision'
+
+bottomContentP = document.querySelectorAll('.bottom-content .text-content p')
+bottomContentP[0].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+bottomContentP[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+bottomContentP[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+//CONTACT
+const contactH4 = document.querySelector('.contact h4')
+contactH4.textContent = 'Contact'
+
+const contactP = document.querySelectorAll('.contact p')
+contactP[0].textContent = '123 Way 456 Street Somewhere, USA'
+contactP[1].textContent = '1 (888) 888-8888'
+contactP[2].textContent = 'sales@greatidea.io'
+
+//FOOTER
+const footerP = document.querySelector('footer p')
+footerP.textContent = 'Copyright Great Idea! 2018'
